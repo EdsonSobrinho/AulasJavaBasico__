74a946ec9734194e2c1ca96ab32bedf48575593d
EdsonSobrinho <edsonpaulosobrinho@gmail.com>
2018-05-02 19:54:41 -0300

    *.*

---
+++ CursoJavaBasico/src/com/loaine/cursojava/aula10/Variáveis.java
@@ -0,0 +1,20 @@
+package com.loaine.cursojava.aula10;
+
+public class Variáveis {
+
+	public static void main(String[] args) {
+		
+		// seguindo a convenção java
+		int idade = 20;
+		String nome = "Edson";
+		String nomeDoMeuCachorro = "totó";
+		String ano2018 = "2018";
+				
+		idade = idade + 16;
+				
+		System.out.println ("Idade = " + idade);
+		System.out.println ("Nome = " + nome);
+		System.out.println ("Ano = " + ano2018);
+	}
+
+}
